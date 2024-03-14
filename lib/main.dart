3130e983c8f56b24c98d0aa8ba00f5de7d3791ae
meliskaradag <85998019+ymeliskaradag@users.noreply.github.com>
2024-03-14 15:03:11 +0300

    list of loan offers updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,231 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class LoanOffer {
+  final String bankName;
+  final double annualExpenseRate;
+  final double interestRate;
+  //final double amount;
+  //final int maturity;
 
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+  LoanOffer({
+    required this.bankName,
+    required this.annualExpenseRate,
+    required this.interestRate,
+    //required this.amount,
+    //required this.maturity,
+  });
+
+  factory LoanOffer.fromJson(Map<String, dynamic> json) {
+  return LoanOffer(
+    bankName: json['bank'],
+    annualExpenseRate: json['annual_rate'],
+    interestRate: json['interest_rate'],
+    //amount: json['amount'],
+    //maturity: json['maturity'],
+  );
   }
+
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class SearchFormScreen extends StatefulWidget {
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _SearchFormScreenState createState() => _SearchFormScreenState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class _SearchFormScreenState extends State<SearchFormScreen> {
+  double amountValue = 1000.0;
+  double maturityValue = 1.0;
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text('Arama Detayları'),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text('Kredi Tutarı: ${amountValue.toStringAsFixed(0)}'),
+            Slider(
+              value: amountValue,
+              min: 1000,
+              max: 300000,
+              divisions: 100,
+              label: amountValue.round().toString(),
+              onChanged: (value) {
+                setState(() {
+                  amountValue = value;
+                });
+              },
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            SizedBox(height: 20),
+            Text('Kredi Vadesi: ${maturityValue.toStringAsFixed(0)}'),
+            Slider(
+              value: maturityValue,
+              min: 1,
+              max: 36,
+              divisions: 36,
+              label: maturityValue.round().toString(),
+              onChanged: (value) {
+                setState(() {
+                  maturityValue = value;
+                });
+              },
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => LoanListingScreen(
+                      amount: amountValue.round().toString(),
+                      maturity: maturityValue.round().toString(),
+                    ),
+                  ),
+                );
+              },
+              child: Text('Hesapla'),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+class LoanListingScreen extends StatefulWidget {
+  final String amount;
+  final String maturity;
+
+  const LoanListingScreen({
+    required this.amount,
+    required this.maturity,
+  });
+
+  @override
+  _LoanListingScreenState createState() => _LoanListingScreenState();
+}
+
+class _LoanListingScreenState extends State<LoanListingScreen> {
+  late Future<List<LoanOffer>> _futureLoanOffers;
+
+  @override
+  void initState() {
+    super.initState();
+    _futureLoanOffers = fetchLoanOffers(widget.amount, widget.maturity);
+  }
+
+  Future<List<LoanOffer>> fetchLoanOffers(String amount, String maturity) async {
+    final response = await http.get(
+      Uri.parse('https://api2.teklifimgelsin.com/api/getLoanOffers?kredi_tipi=0&vade=$maturity&tutar=$amount'),
+    );
+
+    if (response.statusCode == 200) {
+      print(response.body);
+
+      Map<String, dynamic> parsedData = jsonDecode(response.body);
+      List<dynamic> activeOffers = parsedData['active_offers'];
+      List<LoanOffer> _loanOffers = activeOffers.map((offer) => LoanOffer.fromJson(offer)).toList();
+      return _loanOffers;
+    } else {
+      // Handle error
+      throw Exception('Kredi teklifleri yüklenemedi.');
+    }
+
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Kredi Teklifleri'),
+      ),
+      body: FutureBuilder<List<LoanOffer>>(
+        future: _futureLoanOffers,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasError) {
+            return Center(child: Text('Hata: ${snapshot.error}'));
+          } else {
+            List<LoanOffer> _loanOffers = snapshot.data!;
+            return ListView.builder(
+              itemCount: _loanOffers.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text(_loanOffers[index].bankName),
+                  subtitle: Text('Faiz Oranı: ${_loanOffers[index].interestRate.toStringAsFixed(2)}'),
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => LoanDetailScreen(
+                          loanOffer: _loanOffers[index],
+                        ),
+                      ),
+                    );
+                  },
+                );
+              },
+            );
+          }
+        },
+      ),
+    );
+  }
+}
+
+class LoanDetailScreen extends StatelessWidget {
+  final LoanOffer loanOffer;
+
+  const LoanDetailScreen({required this.loanOffer});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Kredi Detayları'),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text('Banka Adı: ${loanOffer.bankName}'),
+            SizedBox(height: 10),
+            Text('Yıllık Maliyet Oranı: ${loanOffer.annualExpenseRate.toStringAsFixed(2)}'),
+            SizedBox(height: 10),
+            Text('Faiz Oranı: ${loanOffer.interestRate.toStringAsFixed(2)}'),
+            SizedBox(height: 10),
+            //Text('Kredi Tutarı: ${loanOffer.amount}'),
+            SizedBox(height: 10),
+            //Text('Kredi Vadesi: ${loanOffer.maturity}'),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Kredi Uygulaması',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: SearchFormScreen(),
+      debugShowCheckedModeBanner: false, 
     );
   }
 }
